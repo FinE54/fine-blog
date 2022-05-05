e10146eaf7486a92ea3214a0f2105603c259792f
Fine <finegn@outlook.com>
2022-05-05 14:08:18 +0800

    调整接口指内部直接调用

--- api/post.js
+++ api/post.js
@@ -1,13 +1,38 @@
 import { defHttp } from "libs/request";
+import {
+  getMarkdownList,
+  getMarkdownContent,
+  getMarkdownTags,
+} from "blogs";
 
-export const getList = (payload) => {
-  return defHttp.get("/api/post", payload);
+export const getList = async ({ page = 1, size = 10, tag }) => {
+  const markdownList = getMarkdownList().filter((item) => {
+    if (!tag) return true;
+    return item.tags.includes(tag);
+  });
+
+  const start = (page - 1) * size;
+  const end = start + size - 1;
+
+  const resList = markdownList.slice(start, end);
+
+  return Promise.resolve({
+    code: 1,
+    data: { list: resList, total: markdownList.length },
+    msg: "",
+  });
 };
 
-export const getPost = (path) => {
-  return defHttp.get("/api/post/" + path);
+export const getPost = async (path) => {
+  const content = getMarkdownContent({ slug: path });
+  return Promise.resolve({ code: 1, data: content, msg: "" });
 };
 
-export const getTags = (path) => {
-  return defHttp.get("/api/tag");
+export const getTags = async (path) => {
+  const tagList = getMarkdownTags();
+  return Promise.resolve({
+    code: 1,
+    data: { list: tagList, total: tagList.length },
+    msg: "",
+  });
 };
--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 const isDev = process.env.NODE_ENV === "development";
 
-const baseUrl = isDev ? "http://localhost:3000" : "https://blog.fine54.com";
+const baseUrl = isDev ? "http://localhost:3000" : "http://localhost:3000";
+// const baseUrl = "";
 const primaryColor = "#5cb78c";
 
 export { isDev, baseUrl, primaryColor };
